cf82ff851f3b530e2b159bdbb48354e118ad32df
Kai Quander <kq1003@gmail.com>
2020-07-30 09:28:27 -0400

    added some pages

---
+++ frontend/src/components/FormPage/Formpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Formpage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Formpage;
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-import actions from '../../services/index'
-
 
 class Home extends Component {
-  async componentDidMount() {
-    //actions.test()
-  }
   render() {
     return (
       <div>
