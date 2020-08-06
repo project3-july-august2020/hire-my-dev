683a85fcad5ed401adb649e71972a27eb88c86bd
Tzikas <niko.tzikas@gmail.com>
2020-08-06 17:47:56 -0400

    uppercase

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 const PLM = require("passport-local-mongoose");
 
-console.log("IN USERS")
+console.log("IN USERS!!!")
 const userSchema = new Schema({
   username: String,
   skills: [String],
@@ -21,6 +21,7 @@ const userSchema = new Schema({
 
   userSchema.plugin(PLM, { usernameField: "email" });
       
+
   
 module.exports = model('User', userSchema);
 
